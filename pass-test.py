2e2be2967296f1121e1fd3e7eca1a801ad84049b
codewithmeena <117627576+codewithmeena@users.noreply.github.com>
2022-11-06 23:21:29 -0800

    Create pass-test.py

---
+++ pass-test.py
@@ -0,0 +1,12 @@
+#WRITE CODE HERE
+from time import sleep
+n=int(input("enter the password : "))
+if n==123456:
+    print("correct ")
+    sleep(2)
+    quit()
+else:
+    print("wrong...............................")
+    sleep(2)
+    quit()
+    
